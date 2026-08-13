571f8214e1a83ca9d63e5ee08523222241a4d4df
Patrick <pvanhalm00@gmail.com>
2026-08-14 00:51:57 +0200

    Add Roadrunner semantic packet data

--- Multiplayer/Networking/Data/Customization/DuctTapeConsumedPacket.cs
+++ Multiplayer/Networking/Data/Customization/DuctTapeConsumedPacket.cs
@@ -20,3 +20,37 @@ public sealed class DuctTapeConsumedPacket : ISerializablePacket
         EmptyTapeItemNetId = reader.ReadUInt16();
     }
 }
+
+public enum RoadrunnerSyncAction : byte
+{
+    Start,
+    Acknowledge,
+    Snapshot,
+}
+
+public sealed class RoadrunnerSyncPacket : ISerializablePacket
+{
+    public ushort GadgetItemNetId;
+    public RoadrunnerSyncAction Action;
+    public int LengthMeters;
+    public float Countup;
+    public bool HasCompleted;
+
+    public void Serialize(BinaryWriter writer)
+    {
+        writer.Write(GadgetItemNetId);
+        writer.Write((byte)Action);
+        writer.Write(LengthMeters);
+        writer.Write(Countup);
+        writer.Write(HasCompleted);
+    }
+
+    public void Deserialize(BinaryReader reader)
+    {
+        GadgetItemNetId = reader.ReadUInt16();
+        Action = (RoadrunnerSyncAction)reader.ReadByte();
+        LengthMeters = reader.ReadInt32();
+        Countup = reader.ReadSingle();
+        HasCompleted = reader.ReadBoolean();
+    }
+}
