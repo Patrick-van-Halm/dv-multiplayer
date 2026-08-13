2946e91ac682a14a8a8590b4c79fbbf903b9f20d
Patrick <pvanhalm00@gmail.com>
2026-08-14 00:15:28 +0200

    Observe gadget placement and removal

--- Multiplayer/Patches/Customization/GadgetItemRegistrationPatch.cs
+++ Multiplayer/Patches/Customization/GadgetItemRegistrationPatch.cs
@@ -1,7 +1,11 @@
+using DV.Customization;
 using DV.Customization.Gadgets;
 using HarmonyLib;
+using Multiplayer.Components.Networking;
+using Multiplayer.Components.Networking.Customization;
 using Multiplayer.Components.Networking.Customization.Gadgets;
 using Multiplayer.Components.Networking.World;
+using Multiplayer.Networking.Data.Customization;
 
 namespace Multiplayer.Patches.Customization;
 
@@ -18,5 +22,46 @@ internal static class GadgetItemRegistrationPatch
         networkedItem.Initialize(__instance);
         GadgetTrackedValueRegistry.Register(networkedItem, __instance, __instance.Gadget);
         networkedItem.FinaliseTrackedValues();
+    }
+}
+
+[HarmonyPatch]
+internal static class GadgetStructuralObservationPatch
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(GadgetItem), nameof(GadgetItem.Place))]
+    private static void AfterPlace(Customization destination, GadgetItem gadgetItem, GadgetBase __result)
+    {
+        if (CustomizationSyncScope.IsApplyingRemote || __result == null || gadgetItem?.Item == null ||
+            !NetworkedItem.TryGetNetworkedItem(gadgetItem.Item, out NetworkedItem networkedItem) || networkedItem.NetId == 0 ||
+            !CustomizationRef.TryFrom(destination, out CustomizationRef target))
+            return;
+
+        if (NetworkLifecycle.Instance.IsHost())
+            GadgetStructuralSync.ClearServerOwnership(networkedItem.NetId);
+
+        GadgetStructuralSync.SendObserved(new GadgetPlacePacket
+        {
+            GadgetItemNetId = networkedItem.NetId,
+            Target = target,
+            LocalPosition = __result.transform.localPosition,
+            LocalRotation = __result.transform.localRotation,
+            IsOnGlass = __result.IsOnGlass,
+        });
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(GadgetBase), nameof(GadgetBase.Remove))]
+    private static void AfterRemove(bool reparentToTrainCar, GadgetItem __result)
+    {
+        if (CustomizationSyncScope.IsApplyingRemote || __result?.Item == null ||
+            !NetworkedItem.TryGetNetworkedItem(__result.Item, out NetworkedItem networkedItem) || networkedItem.NetId == 0)
+            return;
+
+        GadgetStructuralSync.SendObserved(new GadgetRemovePacket
+        {
+            GadgetItemNetId = networkedItem.NetId,
+            ReparentToTrainCar = reparentToTrainCar,
+        });
     }
 }
