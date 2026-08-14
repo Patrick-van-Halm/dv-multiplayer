c01bb3ac85a5cb80f3355b59b5968dda93719701
Patrick <pvanhalm00@gmail.com>
2026-08-14 07:18:18 +0200

    Wire customization action observers

--- Multiplayer/Patches/Customization/CustomizationToolRegistrationPatch.cs
+++ Multiplayer/Patches/Customization/CustomizationToolRegistrationPatch.cs
@@ -1,13 +1,29 @@
 using DV.Customization.Gadgets;
 using DV.Customization.Gadgets.Implementations;
+using DV.Utils;
 using HarmonyLib;
+using Multiplayer.Components.Networking.Customization;
 using Multiplayer.Components.Networking.World;
+using Multiplayer.Networking.Managers.Client;
+using Multiplayer.Networking.Managers.Server;
+using System.Reflection;
+using UnityEngine;
 
 namespace Multiplayer.Patches.Customization;
 
 [HarmonyPatch]
 internal static class CustomizationToolRegistrationPatch
 {
+    private static readonly FieldInfo SolderRemainingField = AccessTools.Field(typeof(GadgetSolderingTool), "remainingUnits");
+    private static readonly MethodInfo SolderUnitsChanged = AccessTools.Method(typeof(GadgetSolderingTool), "OnUnitsChanged");
+
+    private struct DuctTapeUseState
+    {
+        public ushort NetId;
+        public int UsesBefore;
+        public Vector3 Position;
+    }
+
     [HarmonyPostfix]
     [HarmonyPatch(typeof(DuctTape), "Awake")]
     private static void DuctTapeAwake(DuctTape __instance)
@@ -16,10 +32,30 @@ internal static class CustomizationToolRegistrationPatch
         networkedItem.Initialize(__instance);
         networkedItem.RegisterTrackedValue("ductTape.usesLeft", () => __instance.usesLeft, value =>
         {
-            __instance.usesLeft = value;
-            __instance.tapeModelUpdater?.UpdateActiveStates(__instance.PercentageUsesLeft);
+            __instance.usesLeft = Mathf.Clamp(value, 0, __instance.numberOfUses);
+            ThresholdGameObjectActivator updater = __instance.GetComponent<ThresholdGameObjectActivator>();
+            updater?.UpdateActiveStates(__instance.numberOfUses > 0 ? (float)__instance.usesLeft / __instance.numberOfUses : 0f);
         });
         networkedItem.FinaliseTrackedValues();
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(DuctTape), nameof(DuctTape.ConsumeOneUse))]
+    private static void BeforeDuctTapeUse(DuctTape __instance, ref DuctTapeUseState __state)
+    {
+        __state.UsesBefore = __instance.usesLeft;
+        __state.Position = __instance.transform.position;
+        var itemBase = __instance.GetComponent<DV.CabControls.ItemBase>();
+        if (itemBase != null && NetworkedItem.TryGetNetworkedItem(itemBase, out NetworkedItem item))
+            __state.NetId = item.NetId;
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(DuctTape), nameof(DuctTape.ConsumeOneUse))]
+    private static void AfterDuctTapeUse(DuctTapeUseState __state)
+    {
+        if (__state.UsesBefore == 1)
+            DuctTapeTerminalSync.ObserveTerminalUse(__state.NetId, __state.Position);
     }
 
     [HarmonyPostfix]
@@ -28,11 +64,30 @@ internal static class CustomizationToolRegistrationPatch
     {
         NetworkedItem networkedItem = __instance.GetComponent<NetworkedItem>() ?? __instance.gameObject.AddComponent<NetworkedItem>();
         networkedItem.Initialize(__instance);
-        networkedItem.RegisterTrackedValue("soldering.remaining", () => __instance.remainingUnits, value =>
+        if (SolderRemainingField != null)
         {
-            __instance.remainingUnits = value;
-            __instance.OnUnitsChanged();
-        });
+            networkedItem.RegisterTrackedValue("soldering.remaining",
+                () => (int)SolderRemainingField.GetValue(__instance),
+                value =>
+                {
+                    SolderRemainingField.SetValue(__instance, Mathf.Clamp(value, -1, 72089));
+                    SolderUnitsChanged?.Invoke(__instance, null);
+                });
+        }
         networkedItem.FinaliseTrackedValues();
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(NetworkClient), "Subscribe")]
+    private static void SubscribeDuctTapeClient(NetworkClient __instance)
+    {
+        DuctTapeTerminalSync.RegisterClient(__instance);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(NetworkServer), "Subscribe")]
+    private static void SubscribeDuctTapeServer(NetworkServer __instance)
+    {
+        DuctTapeTerminalSync.RegisterServer(__instance);
     }
 }
--- Multiplayer/Patches/Customization/GadgetItemRegistrationPatch.cs
+++ Multiplayer/Patches/Customization/GadgetItemRegistrationPatch.cs
@@ -1,7 +1,21 @@
+using DV.CabControls;
+using DV.Customization;
 using DV.Customization.Gadgets;
+using DV.Customization.Gadgets.Implementations;
+using DV.Items.Snapping;
 using HarmonyLib;
+using Multiplayer.Components.Networking;
+using Multiplayer.Components.Networking.Customization;
 using Multiplayer.Components.Networking.Customization.Gadgets;
 using Multiplayer.Components.Networking.World;
+using Multiplayer.Networking.Data;
+using Multiplayer.Networking.Data.Customization;
+using Multiplayer.Networking.Managers.Client;
+using Multiplayer.Networking.Managers.Server;
+using Multiplayer.Networking.Packets.Serverbound;
+using Multiplayer.Networking.TransportLayers;
+using System;
+using UnityEngine;
 
 namespace Multiplayer.Patches.Customization;
 
@@ -11,12 +25,142 @@ internal static class GadgetItemRegistrationPatch
     [HarmonyPostfix]
     private static void Awake(GadgetItem __instance)
     {
-        if (__instance?.Gadget == null)
+        if (__instance?.Item == null || __instance.Gadget == null)
             return;
-
         NetworkedItem networkedItem = __instance.GetComponent<NetworkedItem>() ?? __instance.gameObject.AddComponent<NetworkedItem>();
         networkedItem.Initialize(__instance);
-        GadgetTrackedValueRegistry.Register(networkedItem, __instance.Gadget);
+        GadgetTrackedValueRegistry.Register(networkedItem, __instance, __instance.Gadget);
         networkedItem.FinaliseTrackedValues();
+    }
+}
+
+[HarmonyPatch]
+internal static class GadgetStructuralObservationPatch
+{
+    [HarmonyPostfix, HarmonyPatch(typeof(GadgetItem), nameof(GadgetItem.Place))]
+    private static void AfterPlace(Customization destination, GadgetItem gadgetItem, GadgetBase __result)
+    {
+        if (CustomizationSyncScope.IsApplyingRemote || __result == null || gadgetItem?.Item == null ||
+            !NetworkedItem.TryGetNetworkedItem(gadgetItem.Item, out NetworkedItem networkedItem) || networkedItem.NetId == 0 ||
+            !CustomizationRef.TryFrom(destination, out CustomizationRef target))
+            return;
+        if (NetworkLifecycle.Instance.IsHost())
+            GadgetStructuralSync.ClearServerOwnership(networkedItem.NetId);
+        GadgetStructuralSync.SendObserved(new GadgetPlacePacket
+        {
+            GadgetItemNetId = networkedItem.NetId,
+            Target = target,
+            LocalPosition = __result.transform.localPosition,
+            LocalRotation = __result.transform.localRotation,
+            IsOnGlass = __result.IsOnGlass,
+        });
+    }
+
+    [HarmonyPrefix, HarmonyPatch(typeof(GadgetBase), nameof(GadgetBase.Remove))]
+    private static void BeforeRemove(ref IDisposable __state)
+    {
+        if (!CustomizationSyncScope.IsApplyingRemote)
+            __state = CustomizationSyncScope.LocalRoot();
+    }
+
+    [HarmonyPostfix, HarmonyPatch(typeof(GadgetBase), nameof(GadgetBase.Remove))]
+    private static void AfterRemove(bool reparentToTrainCar, GadgetItem __result)
+    {
+        if (CustomizationSyncScope.IsApplyingRemote || __result?.Item == null ||
+            !NetworkedItem.TryGetNetworkedItem(__result.Item, out NetworkedItem networkedItem) || networkedItem.NetId == 0)
+            return;
+        GadgetStructuralSync.SendObserved(new GadgetRemovePacket
+        {
+            GadgetItemNetId = networkedItem.NetId,
+            ReparentToTrainCar = reparentToTrainCar,
+        });
+    }
+
+    [HarmonyFinalizer, HarmonyPatch(typeof(GadgetBase), nameof(GadgetBase.Remove))]
+    private static Exception FinishRemove(Exception __exception, IDisposable __state)
+    {
+        __state?.Dispose();
+        return __exception;
+    }
+}
+
+[HarmonyPatch]
+internal static class GadgetSnapObservationPatch
+{
+    [HarmonyPostfix, HarmonyPatch(typeof(ItemSnapPointBase), nameof(ItemSnapPointBase.SnapItem), new[] { typeof(ItemBase), typeof(bool) })]
+    private static void AfterSnap(ItemSnapPointBase __instance, ItemBase itemToSnap, bool __result)
+    {
+        if (!__result || __instance is not SnapPointGadget point || itemToSnap == null ||
+            CustomizationSyncScope.IsApplyingRemote || CustomizationSyncScope.IsApplyingRootAction ||
+            !GadgetSnapSync.TryDescribe(point, out ushort ownerId, out int pointIndex) ||
+            !NetworkedItem.TryGetNetworkedItem(itemToSnap, out NetworkedItem attached) || attached.NetId == 0)
+            return;
+        Transform anchor = itemToSnap.SnappableItem?.GetAnchor(point.SnapPointType);
+        SnapPointAnchorSliding sliding = anchor?.GetComponent<SnapPointAnchorSliding>();
+        GadgetStructuralSync.SendObserved(new GadgetSnapPacket { State = new GadgetSnapState
+        {
+            TargetGadgetItemNetId = ownerId,
+            AttachedItemNetId = attached.NetId,
+            SnapPointIndex = pointIndex,
+            HasSlidingAnchor = sliding != null,
+            SlidingAnchorLocalPosition = sliding != null ? sliding.transform.localPosition : default,
+        }});
+    }
+
+    [HarmonyPrefix, HarmonyPatch(typeof(ItemSnapPointBase), nameof(ItemSnapPointBase.UnsnapItem), new[] { typeof(bool) })]
+    private static void BeforeUnsnap(ItemSnapPointBase __instance, ref ItemBase __state)
+    {
+        __state = __instance is SnapPointGadget ? __instance.SnappedItem : null;
+    }
+
+    [HarmonyPostfix, HarmonyPatch(typeof(ItemSnapPointBase), nameof(ItemSnapPointBase.UnsnapItem), new[] { typeof(bool) })]
+    private static void AfterUnsnap(ItemSnapPointBase __instance, bool __result, ItemBase __state)
+    {
+        if (!__result || __instance is not SnapPointGadget point || __state == null ||
+            CustomizationSyncScope.IsApplyingRemote || CustomizationSyncScope.IsApplyingRootAction ||
+            !GadgetSnapSync.TryDescribe(point, out ushort ownerId, out int pointIndex) ||
+            !NetworkedItem.TryGetNetworkedItem(__state, out NetworkedItem attached) || attached.NetId == 0)
+            return;
+        GadgetStructuralSync.SendObserved(new GadgetUnsnapPacket
+        {
+            TargetGadgetItemNetId = ownerId,
+            AttachedItemNetId = attached.NetId,
+            SnapPointIndex = pointIndex,
+        });
+    }
+}
+
+[HarmonyPatch]
+internal static class RoadrunnerObservationPatch
+{
+    [HarmonyPrefix, HarmonyPatch(typeof(GadgetRoadrunner), "Update")]
+    private static void BeforeUpdate() => RoadrunnerSync.EnterNativeUpdate();
+
+    [HarmonyFinalizer, HarmonyPatch(typeof(GadgetRoadrunner), "Update")]
+    private static Exception AfterUpdate(Exception __exception)
+    {
+        RoadrunnerSync.ExitNativeUpdate();
+        return __exception;
+    }
+
+    [HarmonyPostfix, HarmonyPatch(typeof(GadgetRoadrunner), nameof(GadgetRoadrunner.StartMeasure))]
+    private static void AfterStart(GadgetRoadrunner __instance) => RoadrunnerSync.SendObserved(__instance, RoadrunnerSyncAction.Start);
+
+    [HarmonyPostfix, HarmonyPatch(typeof(GadgetRoadrunner), nameof(GadgetRoadrunner.Acknowledge))]
+    private static void AfterAcknowledge(GadgetRoadrunner __instance) => RoadrunnerSync.SendObserved(__instance, RoadrunnerSyncAction.Acknowledge);
+
+    [HarmonyPostfix, HarmonyPatch(typeof(NetworkClient), "Subscribe")]
+    private static void SubscribeClient(NetworkClient __instance) => RoadrunnerSync.RegisterClient(__instance);
+
+    [HarmonyPostfix, HarmonyPatch(typeof(NetworkServer), "Subscribe")]
+    private static void SubscribeServer(NetworkServer __instance) => RoadrunnerSync.RegisterServer(__instance);
+
+    [HarmonyPrefix, HarmonyPriority(Priority.High), HarmonyPatch(typeof(NetworkServer), "OnServerboundLoadStateUpdatePacket")]
+    private static void SendJoinState(NetworkServer __instance, ServerboundLoadStateUpdatePacket packet, ITransportPeer peer)
+    {
+        if (packet.LoadState == PlayerLoadingState.ReadyForCustomizers &&
+            __instance.TryGetServerPlayer(peer, out ServerPlayer player) &&
+            player.LoadingState == PlayerLoadingState.ReadyForTrainSets)
+            RoadrunnerSync.SendJoinState(__instance, player);
     }
 }
