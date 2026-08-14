272d8fb9ebb042f5d88c040a5034f0d4e0272c7e
Patrick <pvanhalm00@gmail.com>
2026-08-14 07:58:37 +0200

    Send customization packets reliably in order

--- Multiplayer/Components/Networking/Customization/CustomizationPacketSend.cs
+++ Multiplayer/Components/Networking/Customization/CustomizationPacketSend.cs
@@ -2,6 +2,7 @@ using LiteNetLib;
 using MPAPI.Interfaces.Packets;
 using Multiplayer.API;
 using Multiplayer.Networking.Managers;
+using Multiplayer.Networking.Managers.Client;
 using Multiplayer.Networking.Managers.Server;
 using Multiplayer.Networking.TransportLayers;
 using System.Reflection;
@@ -12,6 +13,8 @@ internal static class CustomizationPacketSend
 {
     private static readonly MethodInfo SendSerializable = typeof(NetworkManager).GetMethod(
         "SendNetSerializablePacket", BindingFlags.Instance | BindingFlags.NonPublic);
+    private static readonly MethodInfo SendSerializableToServer = typeof(NetworkClient).GetMethod(
+        "SendNetSerializablePacketToServer", BindingFlags.Instance | BindingFlags.NonPublic);
 
     public static bool SendJoinState<T>(NetworkServer server, ITransportPeer peer, T packet)
         where T : class, ISerializablePacket, new()
@@ -19,9 +22,21 @@ internal static class CustomizationPacketSend
         if (server == null || peer == null || packet == null || SendSerializable == null)
             return false;
 
-        var wrapper = new ExternalSerializablePacketWrapper<T> { Packet = packet };
+        ExternalSerializablePacketWrapper<T> wrapper = new() { Packet = packet };
         SendSerializable.MakeGenericMethod(typeof(ExternalSerializablePacketWrapper<T>)).Invoke(
             server, new object[] { peer, wrapper, DeliveryMethod.ReliableOrdered });
+        return true;
+    }
+
+    public static bool SendToServer<T>(NetworkClient client, T packet)
+        where T : class, ISerializablePacket, new()
+    {
+        if (client == null || packet == null || SendSerializableToServer == null)
+            return false;
+
+        ExternalSerializablePacketWrapper<T> wrapper = new() { Packet = packet };
+        SendSerializableToServer.MakeGenericMethod(typeof(ExternalSerializablePacketWrapper<T>)).Invoke(
+            client, new object[] { wrapper, DeliveryMethod.ReliableOrdered });
         return true;
     }
 }
