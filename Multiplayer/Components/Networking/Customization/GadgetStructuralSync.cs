d8721f37b135d6a48652191a57e6cc4e495fdcf0
Patrick <pvanhalm00@gmail.com>
2026-08-14 07:59:15 +0200

    Order client customization actions to server

--- Multiplayer/Components/Networking/Customization/GadgetStructuralSync.cs
+++ Multiplayer/Components/Networking/Customization/GadgetStructuralSync.cs
@@ -49,7 +49,7 @@ internal static class GadgetStructuralSync
         }
         else
         {
-            lifecycle.Client.SendExternalSerializablePacketToServer(packet, true);
+            CustomizationPacketSend.SendToServer(lifecycle.Client, packet);
         }
     }
 
@@ -62,7 +62,7 @@ internal static class GadgetStructuralSync
         if (lifecycle.IsHost())
             BroadcastPlace(lifecycle.Server, packet, null, item, gadget);
         else
-            lifecycle.Client.SendExternalSerializablePacketToServer(packet, true);
+            CustomizationPacketSend.SendToServer(lifecycle.Client, packet);
     }
 
     public static bool TryGet(ushort itemNetId, out NetworkedItem item, out GadgetItem gadgetItem, out GadgetBase gadget)
