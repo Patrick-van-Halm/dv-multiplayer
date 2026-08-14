7a055e66abb43062cd820c50136ff76b834c99fb
Patrick <pvanhalm00@gmail.com>
2026-08-14 06:43:23 +0200

    Use native customization tool state setters

--- Multiplayer/Patches/Customization/CustomizationToolRegistrationPatch.cs
+++ Multiplayer/Patches/Customization/CustomizationToolRegistrationPatch.cs
@@ -1,19 +1,13 @@
 using DV.Customization.Gadgets;
 using DV.Customization.Gadgets.Implementations;
-using DV.Utils;
 using HarmonyLib;
 using Multiplayer.Components.Networking.World;
-using System.Reflection;
-using UnityEngine;
 
 namespace Multiplayer.Patches.Customization;
 
 [HarmonyPatch]
 internal static class CustomizationToolRegistrationPatch
 {
-    private static readonly FieldInfo SolderRemainingField = AccessTools.Field(typeof(GadgetSolderingTool), "remainingUnits");
-    private static readonly MethodInfo SolderUnitsChanged = AccessTools.Method(typeof(GadgetSolderingTool), "OnUnitsChanged");
-
     [HarmonyPostfix]
     [HarmonyPatch(typeof(DuctTape), "Awake")]
     private static void DuctTapeAwake(DuctTape __instance)
@@ -22,9 +16,8 @@ internal static class CustomizationToolRegistrationPatch
         networkedItem.Initialize(__instance);
         networkedItem.RegisterTrackedValue("ductTape.usesLeft", () => __instance.usesLeft, value =>
         {
-            __instance.usesLeft = Mathf.Clamp(value, 0, __instance.numberOfUses);
-            ThresholdGameObjectActivator updater = __instance.GetComponent<ThresholdGameObjectActivator>();
-            updater?.UpdateActiveStates(__instance.numberOfUses > 0 ? (float)__instance.usesLeft / __instance.numberOfUses : 0f);
+            __instance.usesLeft = value;
+            __instance.tapeModelUpdater?.UpdateActiveStates(__instance.PercentageUsesLeft);
         });
         networkedItem.FinaliseTrackedValues();
     }
@@ -35,16 +28,11 @@ internal static class CustomizationToolRegistrationPatch
     {
         NetworkedItem networkedItem = __instance.GetComponent<NetworkedItem>() ?? __instance.gameObject.AddComponent<NetworkedItem>();
         networkedItem.Initialize(__instance);
-        if (SolderRemainingField != null)
+        networkedItem.RegisterTrackedValue("soldering.remaining", () => __instance.remainingUnits, value =>
         {
-            networkedItem.RegisterTrackedValue("soldering.remaining",
-                () => (int)SolderRemainingField.GetValue(__instance),
-                value =>
-                {
-                    SolderRemainingField.SetValue(__instance, Mathf.Clamp(value, -1, 72089));
-                    SolderUnitsChanged?.Invoke(__instance, null);
-                });
-        }
+            __instance.remainingUnits = value;
+            __instance.OnUnitsChanged();
+        });
         networkedItem.FinaliseTrackedValues();
     }
 }
