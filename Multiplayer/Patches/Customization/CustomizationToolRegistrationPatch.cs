dc89775f7fd0dc621e58845eee38372cf30d7c0f
Patrick <pvanhalm00@gmail.com>
2026-08-14 07:20:08 +0200

    Complete customization tool state registration

--- Multiplayer/Patches/Customization/CustomizationToolRegistrationPatch.cs
+++ Multiplayer/Patches/Customization/CustomizationToolRegistrationPatch.cs
@@ -1,12 +1,10 @@
 using DV.Customization.Gadgets;
 using DV.Customization.Gadgets.Implementations;
-using DV.Utils;
 using HarmonyLib;
 using Multiplayer.Components.Networking.Customization;
 using Multiplayer.Components.Networking.World;
 using Multiplayer.Networking.Managers.Client;
 using Multiplayer.Networking.Managers.Server;
-using System.Reflection;
 using UnityEngine;
 
 namespace Multiplayer.Patches.Customization;
@@ -14,9 +12,6 @@ namespace Multiplayer.Patches.Customization;
 [HarmonyPatch]
 internal static class CustomizationToolRegistrationPatch
 {
-    private static readonly FieldInfo SolderRemainingField = AccessTools.Field(typeof(GadgetSolderingTool), "remainingUnits");
-    private static readonly MethodInfo SolderUnitsChanged = AccessTools.Method(typeof(GadgetSolderingTool), "OnUnitsChanged");
-
     private struct DuctTapeUseState
     {
         public ushort NetId;
@@ -32,9 +27,8 @@ internal static class CustomizationToolRegistrationPatch
         networkedItem.Initialize(__instance);
         networkedItem.RegisterTrackedValue("ductTape.usesLeft", () => __instance.usesLeft, value =>
         {
-            __instance.usesLeft = Mathf.Clamp(value, 0, __instance.numberOfUses);
-            ThresholdGameObjectActivator updater = __instance.GetComponent<ThresholdGameObjectActivator>();
-            updater?.UpdateActiveStates(__instance.numberOfUses > 0 ? (float)__instance.usesLeft / __instance.numberOfUses : 0f);
+            __instance.usesLeft = value;
+            __instance.tapeModelUpdater?.UpdateActiveStates(__instance.PercentageUsesLeft);
         });
         networkedItem.FinaliseTrackedValues();
     }
@@ -45,8 +39,8 @@ internal static class CustomizationToolRegistrationPatch
     {
         __state.UsesBefore = __instance.usesLeft;
         __state.Position = __instance.transform.position;
-        var itemBase = __instance.GetComponent<DV.CabControls.ItemBase>();
-        if (itemBase != null && NetworkedItem.TryGetNetworkedItem(itemBase, out NetworkedItem item))
+        if (__instance.TryGetComponent(out DV.CabControls.ItemBase itemBase) &&
+            NetworkedItem.TryGetNetworkedItem(itemBase, out NetworkedItem item))
             __state.NetId = item.NetId;
     }
 
@@ -64,30 +58,23 @@ internal static class CustomizationToolRegistrationPatch
     {
         NetworkedItem networkedItem = __instance.GetComponent<NetworkedItem>() ?? __instance.gameObject.AddComponent<NetworkedItem>();
         networkedItem.Initialize(__instance);
-        if (SolderRemainingField != null)
+        networkedItem.RegisterTrackedValue("soldering.remaining", () => __instance.remainingUnits, value =>
         {
-            networkedItem.RegisterTrackedValue("soldering.remaining",
-                () => (int)SolderRemainingField.GetValue(__instance),
-                value =>
-                {
-                    SolderRemainingField.SetValue(__instance, Mathf.Clamp(value, -1, 72089));
-                    SolderUnitsChanged?.Invoke(__instance, null);
-                });
-        }
+            __instance.remainingUnits = value;
+            __instance.OnUnitsChanged();
+        });
+        networkedItem.RegisterTrackedValue<uint>("soldering.spool",
+            () => SolderingMagazineSync.GetSpoolNetId(__instance),
+            value => SolderingMagazineSync.ApplyCanonicalSpool(__instance, value),
+            serverAuthoritative: true);
         networkedItem.FinaliseTrackedValues();
     }
 
     [HarmonyPostfix]
     [HarmonyPatch(typeof(NetworkClient), "Subscribe")]
-    private static void SubscribeDuctTapeClient(NetworkClient __instance)
-    {
-        DuctTapeTerminalSync.RegisterClient(__instance);
-    }
+    private static void SubscribeDuctTapeClient(NetworkClient __instance) => DuctTapeTerminalSync.RegisterClient(__instance);
 
     [HarmonyPostfix]
     [HarmonyPatch(typeof(NetworkServer), "Subscribe")]
-    private static void SubscribeDuctTapeServer(NetworkServer __instance)
-    {
-        DuctTapeTerminalSync.RegisterServer(__instance);
-    }
+    private static void SubscribeDuctTapeServer(NetworkServer __instance) => DuctTapeTerminalSync.RegisterServer(__instance);
 }
